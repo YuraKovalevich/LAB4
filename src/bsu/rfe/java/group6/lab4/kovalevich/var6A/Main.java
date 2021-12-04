c5e162dbe81876132de97cff16b8b627d7997b83
Yurochka <kovalevichyura@gmail.com>
2021-12-04 14:42:13 +0300

    finish

--- src/bsu/rfe/java/group6/lab4/kovalevich/var6A/Main.java
+++ src/bsu/rfe/java/group6/lab4/kovalevich/var6A/Main.java
@@ -1,8 +1,13 @@
 package bsu.rfe.java.group6.lab4.kovalevich.var6A;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        //главный экран приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
